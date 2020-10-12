b0726602f906201b628ab9d0f386fe85224728a6
José Luis Pereira <pepelui@outlook.com>
2020-10-12 13:52:41 -0300

    Actualizado modelo de taxon, agregado zonas y habitat. Agregada estadistica horaria

--- api/taxon/controllers/taxon.js
+++ api/taxon/controllers/taxon.js
@@ -9,7 +9,7 @@ const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
   async statistics(ctx) {
-    const months = new Array(12).fill(0);
+    const hoursCount = new Array(24).fill(0)
     let years = {}
     const observations = await strapi.query('observation').find({ 'taxon.id': ctx.params.id });
     const weather = observations.filter(obs => obs.weather != null).map(obs => obs.weather);
@@ -17,15 +17,22 @@ module.exports = {
     observations.forEach(observation => { 
       const date = new Date(observation.date)
       const observationYear = date.getFullYear()
+      const observationHour = date.getHours()
 
       if(!years[observationYear]) {
         years[observationYear] = new Array(12).fill(0);
       }
       years[observationYear][date.getMonth()]++
+      hoursCount[observationHour]++
     })
 
     const data =  {
       date_count: years,
+      time: {
+        hours_count: hoursCount,
+        max: hoursCount.indexOf(Math.max(...hoursCount)),
+        min: hoursCount.indexOf(Math.min.apply(null, hoursCount.filter(Boolean)))
+      },
       temp: {
         min: Math.min(...weather.map(w => w.main.temp)),
         max: Math.max(...weather.map(w => w.main.temp))
