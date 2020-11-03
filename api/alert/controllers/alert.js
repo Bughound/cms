c27a3ffcf32d0f627ae4772a685afd0aaebfeeb0
José Luis Pereira <pepelui@outlook.com>
2020-11-03 18:21:01 -0300

    Agregado cambios en el modelo de alerta

--- api/alert/controllers/alert.js
+++ api/alert/controllers/alert.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.alert.search(ctx.query)
+    } else {
+      entities = await strapi.services.alert.find(ctx.query)
+    }
+
+    const taxonIds = entities.map(e => e.observation.taxon).filter(id => id)
+    console.log(taxonIds)
+    const taxonEntity = await strapi.services.taxon.find({ id_in: taxonIds })
+
+    return entities.map(entity => {
+      entity.observation.taxon = entity.observation.taxon ? (taxonEntity.find(taxon => taxon.id === entity.observation.taxon) || null) : entity.observation.taxon
+      return sanitizeEntity(entity, { model: strapi.models.alert })
+    })
+  },
+};
